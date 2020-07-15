e783d5a059b6d543d37dc71fa6f19506b15e96fd
Paschal <blaizepaschal@gmail.com>
2020-07-15 02:21:43 +0100

    Working App

--- weather/views.py
+++ weather/views.py
@@ -11,52 +11,67 @@ def get_weather(lat, lon, exclude='', unit='metric'):
     weather = requests.get(url, params)
     return weather.json()
 
-def geocoder(location):
+def geocoded(location):
     url = 'https://api.opencagedata.com/geocode/v1/json'
     params = {'q':location, 'key': 'c45cd28450d24dd68b7621cd85165dbd'}
     geocode = requests.get(url, params)
     lat = geocode.json()['results'][-1]['geometry']['lat']
     lon = geocode.json()['results'][-1]['geometry']['lng']
-    pprint(geocode.json()['results'][-1])
-    print(lat, lon)
     return lat, lon
 
-weather = get_weather(*geocoder("Owerri, Nigeria"))
-city = "Owerri"
+def geocoder(location):
+    global city
+    url = 'https://geocode.xyz'
+    params = {"locate": location, 'geoit':"JSON"}
+    r = requests.get(url, params)
+    geocode = r.json()
+    city = geocode['standard']['city']
+    return geocode['latt'], geocode['longt']
+
+weather = get_weather(*geocoder("Abuja, Nigeria"))
+
 
 # Create your views here.
-def home(request):
+def home(request, hour=None):
     global weather
     global city
+    if not hour:
+        focus = weather['current']
 
     if request.method == "POST":
         location= request.POST['location']
-        lat, lon = geocoder(location)
+        #lat, lon = geocoder(location)
         #print(exclude)
-        return 
-        weather = get_weather(lat, lon)
-        return HttpResponseRedirect('/home/')
+        #return 
+        weather = get_weather(*geocoder(location))
+        return HttpResponseRedirect('/weather/')
 
     if request.method == "GET":
-        print(weather['current'])
+        dt =  weather['timezone_offset'] + focus['dt']
+        dt = datetime.fromtimestamp(dt)
+        print(weather['timezone_offset'], focus['dt'])
         result = {}
-        result['time'] = datetime.strftime(datetime.fromtimestamp(weather['current']['dt']), "%a, %I:%M %p")
+        result['time'] = dt.strftime( "%a, %I:%M %p")
+        if int(dt.strftime("%H")) in range(8, 20):
+            result['con'], result['rev'] = "AM", "PM"
+        else:
+            result['con'], result['rev'] = "PM", "AM"
         result['city'] = city
-        result['current_description'] = weather['current']['weather'][0]['description']
-        result['current_temp'] = weather['current']['temp']
-        result['humidity'] = weather["current"]['humidity']
-        result['cloudiness'] = weather['current']['clouds']
-        result['rain'] = weather['current']['rain']['1h'] if weather['current'].get('rain') else "N/A"
-        result['wind'] = weather['current']['wind_speed']
-        result['current_icon'] = weather['current']['weather'][0]['icon']
+        result['current_description'] = focus['weather'][0]['description'].title()
+        result['current_temp'] = round(focus['temp'])
+        result['humidity'] = focus['humidity']
+        result['cloudiness'] = focus['clouds']
+        result['rain'] = str(focus['rain']['1h'])+' mmh' if focus.get('rain') else "N/A"
+        result['wind'] = focus['wind_speed']
+        result['current_icon'] = focus['weather'][0]['icon']
         result['daily'] = []
-        for i, day in enumerate(weather['daily'][1:]):
-            print(day['dt'])
+        for i, day in enumerate(weather['daily']):
+            icon = day['weather'][0]['icon']
             max = round(day['temp']['max'])
             min= round(day['temp']['min'])
             day = datetime.strftime(datetime.fromtimestamp(day['dt']), "%A")
-            result['daily'].append({'day':day ,"min": min, "max": max})
+            result['daily'].append({'day':day ,"min": min, "max": max, "icon": icon})
 
         #return JsonResponse(result)
 
-        return render(request, "weather.html", {'weather': weather})
+        return render(request, "weather.html", {'result': result})
