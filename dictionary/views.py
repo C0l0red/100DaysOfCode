6e6ce9907057f4b5506b8063c9dd35a7b6b415cf
Paschal <blaizepaschal@gmail.com>
2020-07-21 21:41:58 +0100

    Updated UI

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse, HttpResponseRedirect
+import requests
+from django_dictionary.settings import API_KEY
+
+def get_word(word):
+    url = f"https://wordsapiv1.p.rapidapi.com/words/{word}"
+    print(API_KEY) 
+    headers = {
+        'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
+        'x-rapidapi-key': API_KEY
+        }
+    r = requests.get(url, headers=headers)
+    #print(r.json())
+    return r.json()
+
 
 # Create your views here.
+def dictionary(request, word=None):
+    if request.GET.get('word'):
+        return redirect("dictionary", word= request.GET['word'])
+
+    if word:
+        request.session["result"] = get_word(word)
+
+    if "result" not in request.session:
+        request.session['result'] = get_word('example')
+
+    result = request.session['result']
+    
+
+    return render(request, 'dictionary.html' , {'Result': result})
+    #return JsonResponse(result)
--- django_dictionary/urls.py
+++ django_dictionary/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dictionary.views import dictionary
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dictionary/<str:word>', dictionary, name="dictionary"),
+    path('dictionary/', dictionary),
 ]
