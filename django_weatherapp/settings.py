a527f2cfd98e9da9ddc4382885e56d609200c519
Paschal <blaizepaschal@gmail.com>
2021-04-16 08:15:35 +0100

    Latest update

--- django_weatherapp/settings.py
+++ django_weatherapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_g=_47gi1^50bvv(g-ixp6nk@h#*hh!zx0i_5d1d+j*lx&6r@('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
